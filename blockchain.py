f106caaf6699523941a825721652a9082a3ec2ce
Paloma Pérez de Madrid <paloma.perezlaguna@beca.ceu.es>
2024-07-28 18:15:49 +0200

    crear cadena de bloque completado

--- blockchain.py
+++ blockchain.py
@@ -20,6 +20,7 @@ class Blockchain:
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
         # proof --> identificador del bloque
+      
         
     def create_block(self, proof, previous_hash):
         block = {
@@ -31,9 +32,11 @@ class Blockchain:
             
         self.chain.append(block)
         return block
-        
+      
+    
     def get_previous_block(self):
         return self.chain[-1]
+    
     
     # Proof-of-work --> el nonce que los mineros tienen que encontrar
     def proof_of_work(self, previous_proof):
@@ -43,8 +46,7 @@ class Blockchain:
         while check_proof is False:
             # No hacer esta operación simétrica!!
             number_new_prev_proof = str((new_proof**2 - previous_proof**3)**(1/5)) # Num que implique el new proof y el prev proof 
-            
-            hash_operation = hashlib.sha256(number_new_prev_proof.encode().hexdigest())
+            hash_operation = hashlib.sha256(number_new_prev_proof.encode()).hexdigest()
             
             # Revisar si tiene 4 ceros en las primeras posiciones
             if hash_operation[:4] =='0000':
@@ -52,9 +54,42 @@ class Blockchain:
             else:
                 new_proof += 1
         return new_proof
+      
+    
+    # Crear el hash de un bloque
+    def hash(self, block):
+        # json se puede codificar directamente en un fromato binario para haslib.sha256
+        encoded_block = json.dumps(block, sort_keys=True ).encode() # 2º arg --> ordenadar alfabéticamente, nos aseguramos que la info viene siempre en el mismo orden (si el orden cambia el hash cambia, efecto avalancha, no nos interesa)
+        return hashlib.sha256(encoded_block).hexdigest()
         
+        
+    # Validar el contenido de nuestro bloque es correcto (buen hash)
+    #   para cada bloque --> prev_hash = hash bloque anterior y comprobar la proof (nonce, supera la ecuación)
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1 # se irá actualizando y comprobará el 1 con el 2, el 2 con el 3, ...
+        while block_index < len(chain):
+            
+            # Comprobar los previous hashes
+            current_block = chain[block_index]
+            if current_block['previous_hash'] != self.hash(previous_block):
+                return False
+            
+            # Comprobar que el proof (que devuelva 4 ceros)
+            previous_proof = previous_block['proof']
+            current_proof = current_block['proof']
+            # calcular la operación de hash
+            number_current_prev_proof = str((current_proof**2 - previous_proof**3)**(1/5))
+            hash_operation = hashlib.sha256(number_current_prev_proof.encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            
+            # Actualizar las variables del bucle
+            previous_block = current_block
+            block_index += 1
+            
+        return True
+            
         
         
 # Parte 2 - Minado de un bloque de la cadena
-
-

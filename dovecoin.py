cfb6a8908e86609a5760325852aa0b1ba7bc5c20
Paloma Pérez de Madrid <paloma.perezlaguna@beca.ceu.es>
2024-09-14 10:48:56 +0200

    empezando dovecoin

--- dovecoin.py
+++ dovecoin.py
@@ -1,25 +1,29 @@
 """
 @author: pperezdemadrid
-Crear una cadena de bloques
+Crear un criptomoneda
 """
-
-# Cliente HTTP Postman: https://www.getpostman.com/ (descargar app)
+# Cliente HTTP Postman: https://www.getpostman.com/
 
 import datetime
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 import hashlib
+import requests
+from uuid import uuid4
+from urllib.parse import urlparse
+
+# request de flask --> conectarse a un nodo u otro de la cadena de bloques descentralizada
 
 # Parte 1: Crear la cadena de bloques
 class Blockchain:
-    
+
     # Constructor
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
         # proof --> identificador del bloque
-      
-        
+
+
     def create_block(self, proof, previous_hash):
         block = {
                 'index': len(self.chain)+1 , # Así bloque genesis es el 1
@@ -27,52 +31,52 @@ class Blockchain:
                 'proof': proof, # el Nonce!
                 'previous_hash': previous_hash
             }
-            
+
         self.chain.append(block)
         return block
-      
-    
+
+
     def get_previous_block(self):
         return self.chain[-1]
-    
-    
+
+
     # Proof-of-work --> el nonce que los mineros tienen que encontrar
     def proof_of_work(self, previous_proof):
-        new_proof = 1 
+        new_proof = 1
         check_proof = False # si resuelve el problema criptográfico
-        
+
         while check_proof is False:
             # No hacer esta operación simétrica!!
-            number_new_prev_proof = str((new_proof**2 - previous_proof**3)**(1/5)) # Num que implique el new proof y el prev proof 
+            number_new_prev_proof = str((new_proof**2 - previous_proof**3)**(1/5)) # Num que implique el new proof y el prev proof
             hash_operation = hashlib.sha256(number_new_prev_proof.encode()).hexdigest()
-            
+
             # Revisar si tiene 4 ceros en las primeras posiciones
             if hash_operation[:4] =='0000':
                 check_proof = True
             else:
                 new_proof += 1
         return new_proof
-      
-    
+
+
     # Crear el hash de un bloque
     def hash(self, block):
         # json se puede codificar directamente en un formato binario para haslib.sha256
         encoded_block = json.dumps(block, sort_keys=True).encode() # 2º arg --> ordenadar alfabéticamente, nos aseguramos que la info viene siempre en el mismo orden (si el orden cambia el hash cambia, efecto avalancha, no nos interesa)
         return hashlib.sha256(encoded_block).hexdigest()
-        
-        
+
+
     # Validar el contenido de nuestro bloque es correcto (buen hash)
     #   para cada bloque --> prev_hash = hash bloque anterior y comprobar la proof (nonce, supera la ecuación)
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1 # se irá actualizando y comprobará el 1 con el 2, el 2 con el 3, ...
         while block_index < len(chain):
-            
+
             # Comprobar los previous hashes
             current_block = chain[block_index]
             if current_block['previous_hash'] != self.hash(previous_block):
                 return False
-            
+
             # Comprobar que el proof (que devuelva 4 ceros)
             previous_proof = previous_block['proof']
             current_proof = current_block['proof']
@@ -81,15 +85,15 @@ class Blockchain:
             hash_operation = hashlib.sha256(number_current_prev_proof.encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
-            
+
             # Actualizar las variables del bucle
             previous_block = current_block
             block_index += 1
-            
+
         return True
-            
-        
-        
+
+
+
 # Parte 2 - Minado de un bloque de la cadena
 
 # Crear una aplicación web
@@ -107,11 +111,11 @@ def mine_block():
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
-    
+
     # 2) Crear el bloque --> param: proof y prev hash
     previous_hash = blockchain.hash(previous_block)
     block = blockchain.create_block(proof, previous_hash)
-    
+
     # 3) Mostrar la info en postman
     response = {
         'message': '¡Enhorabuena! Has minado un nuevo bloque',
@@ -120,7 +124,7 @@ def mine_block():
         'proof': block['proof'], # el Nonce!
         'previous_hash': block['previous_hash']
         }
-    
+
     return jsonify(response) , 200 # pasarlo a JSON + código
 
 # Obtener la cadena de bloques al completo
@@ -146,30 +150,7 @@ def is_valid():
 # Ejecutar la app
 app.run(host='0.0.0.0', port=5000, debug=True)
 
-"""
-POSTMAN
-* GET: http://127.0.0.1:5000/get_chain 
-* Respuesta: {
-    "chain": [
-        {
-            "index": 1,
-            "previous_hash": "0",
-            "proof": 1,
-            "timestamp": "2024-07-29 10:22:19.819831"
-        }
-    ],
-    "length": 1
-}
 
-* GET: http://127.0.0.1:5000/mine_block
-* Respuesta: {
-    "index": 2,
-    "message": "¡Enhorabuena! Has minado un nuevo bloque",
-    "previous_hash": "9e6b9bbeaa51489dc9b59bd9e2ae8d36024ea9c001d47d09932909c9b1af0bc6",
-    "proof": 2875,
-    "timestamp": "2024-07-29 10:25:56.457138"
-}
-"""
 
 
 
